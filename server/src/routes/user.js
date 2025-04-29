19430007709da3f259fe66ebfba1d438b194372f
Siddhant Godbole <132195350+SiddhantGodboleGit@users.noreply.github.com>
2025-04-30 02:01:52 +0530

    Update user.js

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -23,3 +23,25 @@ router.get('/fetchProfile', async (req, res) => {
     }
 }
 );
+
+router.post('/saveProfile', async (req, res) => {
+    const acuid = req.user.customClaims.acuid;
+    const { display_name, bio, hobby_rating } = req.body;
+
+    try {
+        const updateUserQuery = `
+            UPDATE users
+            SET display_name = $1, bio = $2, hobby_rating = $3
+            WHERE user_id = $4
+        `;
+        const updateUserValues = [display_name, bio, hobby_rating, acuid];
+        await pool.query(updateUserQuery, updateUserValues);
+
+        // Send a success response
+        res.status(200).json({ message: 'User profile updated successfully' });
+    } catch (error) {
+        console.error('Error updating user profile:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+);
