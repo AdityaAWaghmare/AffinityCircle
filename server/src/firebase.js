14dd8b0aa99632ae87bafb421e08a41689f6db62
Aditya Waghmare <adi2004waghmare@gmail.com>
2025-04-29 18:30:19 +0530

    Added Firebase auth middleware

---
+++ server/src/firebase.js
@@ -0,0 +1,8 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../affinity-circle-firebase-adminsdk-fbsvc-78a65fee55.json'); // downloaded key
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = admin;
